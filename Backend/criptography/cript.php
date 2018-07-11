47f788942d854bd5ea9e051d0e15a496c1a63661
gabrielgear <gabriel_ghellere@hotmail.com>
2018-07-11 13:34:45 -0300

    criada classe de criptografia do php

--- Backend/criptography/cript.php
+++ Backend/criptography/cript.php
@@ -1,4 +1,4 @@
-<?php 
+ <!-- <?php 
     $myfile = fopen("newfile.txt", "w") or die("Unable to open file!"); //query pra inserir
 
     $binanceKey = "t16ZOkbUCetVG9FZyRl949WRHAYmOSJiBUjVVcanvznZWlzWRfpBa9V8fLfAX5XB"; 
@@ -13,6 +13,31 @@
     $dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $decod , MCRYPT_MODE_CBC, $iv);
     echo("\n decod \n");
     echo($dec);
-?>
+ ?>  -->
 
+<?php
+class Criptografia{
+    private $binancekey;
+    private $oprSecret;
+    //construtor da classe
+    function Criptografia($key, $secret){
+        $this-> $binanceKey = $key;
+        $this-> $oprSecret = $secret;
+    }
+
+    function criptografa(){
+        $key = substr($this->$binanceKey, 0, 32); 
+        $iv = substr($this->$binanceKey, -16); 
+        $enc = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $this->$oprSecret, MCRYPT_MODE_CBC, $iv));
+        return $enc;
+    }
+
+    function decriptografa(){
+        $key = substr($this->$binanceKey, 0, 32); 
+        $iv = substr($this->$binanceKey, -16); 
+        $decod = base64_decode($this-> $oprSecret);
+        $dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $decod , MCRYPT_MODE_CBC, $iv);
+        return $dec;
+    }
+} ?>
 
