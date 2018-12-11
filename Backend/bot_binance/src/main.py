127541af7a28b1f8bca599c4a65d35a9ce29827f
gabsgear <gabriel_ghellere@hotmail.com>
2018-12-11 21:22:46 -0200

    sdq

---
+++ Backend/bot_binance/src/main.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+import binance_
+import strategies
+import helpers
+import botconfig
+import sys
+import routines
+import time
+from binance.client import Client 
+
+def main():
+    db = botconfig.Db()
+    bot_id = sys.argv[1]
+    global bot_config
+    bot_config = db.getConfigBot(bot_id)
+    db.setPID(bot_id)
+    while(bot_config['active'] != 2):
+        routine(bot_id)
+
+def routine(bot_id):
+    db = botconfig.Db()
+    routine = routines.Routines()
+    bot_config = db.getConfigBot(bot_id)
+    routine.startBuyRoutine(bot_config)
+    routine.startSellRoutine(bot_config)
+
+main()
